c225bbedeaa0261e20605960f7c1c67178a07584
mg.Jung <mg.jung@zeroworks.co.kr>
2026-07-14 09:27:08 +0900

    ranger camera launch file

---
+++ launch/dual_camera.launch.py
@@ -0,0 +1,81 @@
+# Copyright 2018 Lucas Walter
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Lucas Walter nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import os
+from pathlib import Path  # noqa: E402
+import sys
+
+# Hack to get relative import of .camera_config file working
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path)
+
+from camera_config import CameraConfig, USB_CAM_DIR  # noqa: E402
+
+from launch import LaunchDescription  # noqa: E402
+from launch.actions import GroupAction  # noqa: E402
+from launch_ros.actions import Node  # noqa: E402
+
+
+# Two cameras bound via udev rules to /dev/video_cam1 and /dev/video_cam2
+CAMERAS = []
+CAMERAS.append(
+    CameraConfig(
+        name='camera1',
+        param_path=Path(USB_CAM_DIR, 'config', 'params_cam1.yaml')
+    )
+)
+CAMERAS.append(
+    CameraConfig(
+        name='camera2',
+        param_path=Path(USB_CAM_DIR, 'config', 'params_cam2.yaml')
+    )
+)
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    camera_nodes = [
+        Node(
+            package='usb_cam', executable='usb_cam_node_exe', output='screen',
+            name=camera.name,
+            namespace=camera.namespace,
+            parameters=[camera.param_path],
+            remappings=camera.remappings
+        )
+        for camera in CAMERAS
+    ]
+
+    camera_group = GroupAction(camera_nodes)
+
+    ld.add_action(camera_group)
+    return ld
